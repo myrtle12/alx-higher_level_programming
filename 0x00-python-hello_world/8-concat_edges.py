ac17235a9c6bfd9b63d5eb86c7eb31e0aa41a73a
Esther <estheroginni@gmail.com>
2022-05-30 19:34:03 -0700

    string concatenation

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with every clear syntax"
+str = "object-oriented"+" "+"programming"+" "+"with"+" "+"Python"
+print(str)
