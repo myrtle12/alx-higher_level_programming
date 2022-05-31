daacb8b1dc494f37d4d5c03b192fb970f7def799
Esther <estheroginni@gmail.com>
2022-05-31 16:23:40 -0700

    converts integer to string to get index and then to integer for operands

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 num_str = repr(number)
-last_digit_str = num_str[-1]
-last_digit = int(last_digit_str)
+last_str = num_str[-1]
+last_digit = int(last_str)
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is less than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6 and last_digit != 0:
