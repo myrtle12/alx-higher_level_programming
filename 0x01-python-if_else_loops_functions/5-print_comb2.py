01a692aed1ddffe857d6ba57a2f4705236c4ae56
Esther <estheroginni@gmail.com>
2022-05-31 19:17:29 -0700

    2 digit integer

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i > 0 and i < 100:
-        print(", ", end="")
-    print("{:02d}".format(i), end="")
+for i in range(0, 100):
+    if i == 99:
+        print("99")
+    else:
+        print("{:02d}, ".format(i), end="")
