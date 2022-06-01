bd32cae7ffbfffbe2a108b7ac1f73af3148e4900
Esther <estheroginni@gmail.com>
2022-05-31 17:03:52 -0700

    converts integer to string to get index and then to integer for operands

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,10 @@ if number < 0:
     last_digit = last_digit * -1
 else:
     last_digit = number % 10
+    print(f"The last digit of {number}, end=' '")
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"is {last_digit} and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"is {last_digit} and is less than 6 and not 0")
